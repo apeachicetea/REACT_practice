7e9437784a7a2397949a71724f03a85bc6dd0572
kimdaeyoon <89075605+apeachicetea@users.noreply.github.com>
2021-10-23 01:03:51 +0900

    day-1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '아이스티 맛집']);
   let [따봉, 따봉변경] = useState(0);
   //따봉변경(따봉을 대체할 데이터)
+  let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
+
+  function 모달변경함수() {
+    modal변경(!modal);
+  }
+  
   function 함수() {
     따봉변경(따봉 + 1);
   }
@@ -24,28 +31,52 @@ function App() {
   
   return (
     <div className="App">
+
       <div className="black-nav">
         <div>개발 Blog</div>
       </div>
       <button onClick={ 제목변경함수 }>버튼</button>
-      <div className="list">
-        <h3> { 글제목[0] } <span onClick={ 함수 }>🤙🏿</span> { 따봉 } </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>
-      <div className="list">
-        <h3> { 글제목[1] } </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>  
-      <div className="list">
-        <h3> { 글제목[2] } </h3>
-        <p>2월 17일 발행</p>
-        <hr />
-      </div>  
+
+      { 
+      
+        글제목.map(function(el, i) {
+          return (
+            <div className="list">
+              <h3 onClick={ ()=>{ 누른제목변경(i) } }> { el } <span onClick={ 함수 }>🤙🏿</span> { 따봉 } </h3>
+              <p>2월 17일 발행</p>
+              <hr />
+            </div>
+          )
+        })
+      
+      }
+      <button onClick={ ()=>{ 누른제목변경(0) } }>버튼1</button>
+      <button onClick={ ()=>{ 누른제목변경(1) } }>버튼2</button>
+      <button onClick={ ()=>{ 누른제목변경(2) } }>버튼3</button>
+
+      <button onClick={ 모달변경함수 }>열고닫기</button>
+
+
+      {
+        modal === true
+        ? <Modal 글제목 = { 글제목 } 누른제목 = { 누른제목 } />
+        : null
+        //텅빈 HTML이라는 뜻
+      }
 
     </div>
   );
 }
+
+function Modal(props) {
+  return(
+    <div className="modal">
+      <h2>제목 { props.글제목[props.누른제목] }</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  );
+}
+
 
 export default App;
